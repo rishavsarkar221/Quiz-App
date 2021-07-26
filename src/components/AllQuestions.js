5dc0e397b79daab61cbb0f5dcd2079127f277d4e
Rishav Sarkar <76059331+rishavsarkar221@users.noreply.github.com>
2021-07-26 12:01:00 +0530

    Add files via upload

---
+++ src/components/AllQuestions.js
@@ -0,0 +1,59 @@
+const questions = [
+
+    {
+        question: 'Capital of India?',
+        options: ['Delhi', 'Mumbai', 'Calcutta', 'Jaipur'],
+        correct: 'Delhi',
+        points: 2
+    },
+    {
+        question: 'Is Rishav the worst programmer',
+        options: ['yes', 'no'],
+        correct: 'yes',
+        points: 5
+    },
+    {
+        question: 'What is the meaning of Katon Goukakku no Jutsu!',
+        options: ['Multi-Shadow Clone Jutsu', 'Healing Jutsu', 'Fire Style! Fire Ball Jutsu!'],
+        correct: 'Fire Style! Fire Ball Jutsu!',
+        points: 3
+    },
+    {
+        question: 'Is Assembly Easy',
+        options: ['Yes', 'No'],
+        correct: 'No',
+        points: 3
+    },
+    {
+        question: 'How do you print hello world in java?',
+        options: ['console.log("Hello World")', 'System.out.println("Hello World")', 'System.out.print("Hello World")', 'print("Hello World")'],
+        correct: 'System.out.println("Hello World")',
+        points: 3
+    },
+    {
+        question: 'Cell references in which cell locations change relative to their locations copied is: _____',
+        options: ['Absolute Referencing', 'Relative Referencing', 'Mixed Referencing'],
+        correct: 'Relative Referencing',
+        points: 2
+    },
+    {
+        question: "It's so cold outside, I'm alone I'm alright.... This line is from which song",
+        options: ['Cold', "it's cold", "In the End"],
+        correct: 'Cold',
+        points: 3
+    },
+    {
+        question: 'National Bird of India',
+        options: ['Humming Bird', 'Peacock'],
+        correct: 'Peacock',
+        points: 3
+    },
+    {
+        question: 'Which Game Developer made the game Prince of Persia the Shadow and the Flames',
+        options: ['Ubisoft', 'Rockstar Games', 'Broderbund', 'Jordan Mechner'],
+        correct: 'Jordan Mechner',
+        points: 1
+    }
+]
+
+export default questions;
